9d655af36bf0e1a17216ec6f816ce23674af5ebb
Reda Belhaj <redabelhaj98@gmail.com>
2020-04-27 18:17:35 +0000

    Initialisation des fichiers train,get_data,resnetclass

---
+++ get_data.py
@@ -0,0 +1,10 @@
+def get_data(resol_max,train_size, test_size):
+    # get_data(resmax,train_size, test_size)
+    # getst the data a la resolution donnee par resol_max
+    
+    pass
+
+
+
+
+## train ## attention il faut la bonne truc
---
+++ resnetclass.py
@@ -0,0 +1,104 @@
+## A ecrire : 
+## classe ResNet  __init__(self, resolution, width, depth) => dans le forward, mettre un resize :)
+
+
+import torch
+import torch.nn as nn 
+import torch.nn.functional as F
+
+
+class ResNetBlock(nn.Module):
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(ResNetBlock, self).__init__()
+        self.stride = stride
+        self.in_planes=in_planes
+        self.planes = planes
+        if stride!=1:
+
+          self.fx = nn.Sequential(nn.Conv2d(in_planes, planes, 3, stride=2, 
+                                            padding=1),
+                                  nn.ReLU(), 
+                                  nn.Conv2d(planes, planes,3, padding=1))
+          
+          self.iden = nn.Conv2d(self.in_planes, self.planes, 2, stride = 2)
+
+
+        else:
+          self.fx = nn.Sequential(nn.Conv2d(planes, planes, 3, padding = 1),
+                                  nn.ReLU(), 
+                                  nn.Conv2d(planes, planes,3, padding=1))
+          
+          self.iden = nn.Sequential()
+          
+
+
+    def forward(self, x):
+
+      if self.stride ==1:
+        fx = self.fx(x)
+        out = fx + self.iden(x)
+        return F.relu(out)
+
+      else:
+
+        fx = self.fx(x)
+        out = fx + self.iden(x)
+        return F.relu(out)
+
+
+
+
+class ResNet(nn.Module):
+
+  def __init__(self, depth,resolution,width=16, num_classes=10,input_dim=3,Block = ResNetBlock):
+      super(ResNet, self).__init__()
+      self.in_planes = width
+      self.conv1 = nn.Conv2d(input_dim, width, kernel_size=3, stride=1, padding=1, bias=False)
+      self.bn1 = nn.BatchNorm2d(width)
+      self.depth = depth
+      self.width = width
+      self.resolution = resolution
+      
+      resol = resolution
+      # construction des piles
+      plane = width
+      layers = []
+      for nb in depth:
+        layer = self._make_layer(Block,plane ,nb,2)
+        layers.append(layer)
+        plane*=2
+        resol/=2
+      
+      self.layers = nn.Sequential(*layers)
+      # arrivé ici on en est à plane@resol*resol
+      # on applique le pooling
+      self.pool = nn.AvgPool2d(4)
+      resol = int(resol/4)
+
+      out_size = resol*resol*plane 
+      self.linear = nn.Linear(out_size, num_classes)
+
+  def _make_layer(self, Block, planes, num_blocks, stride):
+      layers = []
+      block1 = Block(planes, 2*planes, stride = 2)
+      planes *=2
+      layers.append(block1)
+      for i in range(1,num_blocks):
+        block_new = Block(planes, planes, stride =1)
+        layers.append(block_new)
+      return nn.Sequential(*layers)
+
+  def forward(self, x):
+      
+    # premiere etape
+      out = F.relu(self.bn1(self.conv1(x)))
+     
+      # passage par les piles 
+      out = self.layers(out)
+      
+      # output
+      out = self.pool(out)
+      out = out.view(out.size(0), -1)
+      out = self.linear(out)
+      return out
