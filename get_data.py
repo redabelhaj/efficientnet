50b59ed3138a72be192f9e46f8b55e70689ec759
Reda Belhaj <redabelhaj98@gmail.com>
2020-04-27 20:01:36 +0000

    Fin des modids de get_data, train

--- get_data.py
+++ get_data.py
@@ -1,10 +1,26 @@
-def get_data(resol_max,train_size, test_size):
-    # get_data(resmax,train_size, test_size)
-    # getst the data a la resolution donnee par resol_max
-    
-    pass
+import torchvision
+import torchvision.transforms as transforms
+import torch
+import numpy as np
+
+def get_data(resol_max,train_size, test_size,path, database = 'cifar10'):
+
+    transform = transforms.Compose([transforms.Resize(size=(resol_max, resol_max)), transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))])
+    if database=='cifar10':
+        trainset = torchvision.datasets.CIFAR10(root= path, train=True, transform=transform, download=False)
+        testset = torchvision.datasets.CIFAR10(root = path, train=False,transform=transform, download=False)
+    else:
+        raise NotImplementedError
+    return trainset, testset
 
 
+def get_loader_from_data(data,resol_cible, batch_size, num_workers=2):
+
+    transform = transforms.Resize(size = (resol_cible, resol_cible))
+    data = transform(data)
+
+    loader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=True,num_workers = num_workers)
+    return loader
 
 
-## train ## attention il faut la bonne truc
--- train.py
+++ train.py
@@ -1,2 +1,58 @@
-def train():
-    pass
+from get_data import get_loader_from_data
+import tqdm
+import torch
+
+def train(model, optimizer, trainset, loss_fn, batch_size=64, val_split = .10, n_epoch = 5, cuda=True, disp_stats = True):
+    total_nb_ex = len(trainset)
+    train_idx = int((1-val_split)*total_nb_ex)
+
+    trainset = trainset[:train_idx]
+    valset = trainset[train_idx:]
+
+    resol_cible = model.resolution
+    trainloader = get_loader_from_data(trainset, resol_cible, batch_size = batch_size)
+
+    valloader = get_loader_from_data(valset, resol_cible, batch_size=batch_size)
+
+
+    loss_train, loss_val = [], []
+    #acc_train, acc_val = [],[] ## fct accuracy non encore codée
+
+    loss_func = loss_fn()
+    
+    for epoch in tqdm.tqdm(range(n_epoch)):
+        running_trainloss = 0
+        for data in tqdm.tqdm(trainloader):
+            inputs, labels = data
+            if cuda:
+                inputs = inputs.to('cuda') 
+                labels = labels.to('cuda')
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = loss_func(outputs, labels)
+            loss.backward()
+            running_trainloss+= loss
+            optimizer.step()
+    
+        # maintenant plus qu'a calculer les stats et les afficher
+        loss_train.append(running_trainloss)
+        #acc_train = ## il faut la fonction accuracy
+
+        vloss = 0
+        for data in valloader:
+            ipt, lbl = data
+            if cuda : 
+                ipt = ipt.to('cuda')
+                lbl = ipt.to('cuda')
+            out = model(ipt)
+
+            vloss+= loss_func(out, lbl)
+        loss_val.append(vloss)
+        print('Epoch', epoch, '/', n_epoch, ':')
+        print('train loss : ', running_trainloss)
+        print('val loss : ', vloss)
+        # print('train acc' ) etc....
+
+# a modifier s'il faut retourner les accuracys
+    return model, loss_train, loss_val
+
